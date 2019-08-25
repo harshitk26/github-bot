48f7e8eea632696493c514af8f30e28da1ac0930
Harshit <harshiit.2602@gmail.com>
2019-08-26 01:50:25 +0530

    feat(bot) : updating http response and storing contributor name in set

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var Set = require("collections/set");
+
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
@@ -5,12 +8,41 @@
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
+  var contributors = new Set();
 
   app.on('issues.opened', async context => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     return context.github.issues.createComment(issueComment)
   })
 
+  app.on('push', async context => {
+    // Code was pushed to the repo, what should we do with it?
+    const payload = context.payload
+    contributors.add(payload.pusher.name)
+    app.log('Committed by --- ', payload.pusher.name)
+  })
+
+  // Get an express router to expose new HTTP endpoints
+  const router = app.route('/github')
+
+  // Use any middleware
+  router.use(require('express').static('public'))
+
+  // Add a new route
+  router.get('/contributor', (req, res) => {
+    // res.send('Hello World')
+    for (var user of contributors) {
+        res.write(user+'\n');
+    }
+    res.send()
+  })
+
+  app.route().get('/hello-world', (req, res) => {
+    req.log('Someone is saying hello')
+    for (var user of contributors) {
+        console.log(user+' iterated');
+    }
+  })
   // For more information on building apps:
   // https://probot.github.io/docs/
 
